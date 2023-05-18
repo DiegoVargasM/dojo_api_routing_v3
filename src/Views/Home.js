4cbd900c42c9f9ac4fd14c500298fef967f41ca9
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-18 12:26:58 -0500

    Create API resources state

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { useState } from "react";
 
 export const Home = () => {
+  // 1. Creamos una variable de estado array para
+  // guardar los recursos de la API
+  const [resources, setResources] = useState([]);
+
   return <div>Home</div>;
 };

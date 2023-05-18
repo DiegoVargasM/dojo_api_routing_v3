f6e9dbd52b2a1cde8a329d98124b4e81555c776c
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-18 12:51:32 -0500

    Define async func, add it to useEffect

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getAllResources } from "../services/getswapi-service";
 
 export const Home = () => {
   // 1. Creamos una variable de estado array para
   // guardar los recursos de la API
   const [resources, setResources] = useState([]);
+
+  // 2. Definimos una funcion de flecha asincrona para
+  // traer los datos de la API y asignarlos a nuestra variable
+  const getSwapi = async () => {
+    try {
+      const swapiData = await getAllResources();
+      console.log("copydata", swapiData);
+      // el método '.keys' de los objetos, nos permiten extraer solo
+      // la key del par key:value de un objeto
+      setResources(Object.keys(swapiData.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // 3. Hacemos uso del gancho useEffect para llamar a nuestra funcion async
+  // solamente cuando se carga la pagina por primera vez.
+  // (por eso los [] a final)
+  useEffect(() => {
+    getSwapi();
+  });
 
   return <div>Home</div>;
 };

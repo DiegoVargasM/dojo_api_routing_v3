9e4279fa97671c159d25d787b283d7b196ada7a6
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-18 12:54:57 -0500

    Create Home JSX elements

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,30 +3,40 @@ import { useState, useEffect } from "react";
 import { getAllResources } from "../services/getswapi-service";
 
 export const Home = () => {
-  // 1. Creamos una variable de estado array para
-  // guardar los recursos de la API
-  const [resources, setResources] = useState([]);
+	// 1. Creamos una variable de estado array para
+	// guardar los recursos de la API
+	const [resources, setResources] = useState([]);
 
-  // 2. Definimos una funcion de flecha asincrona para
-  // traer los datos de la API y asignarlos a nuestra variable
-  const getSwapi = async () => {
-    try {
-      const swapiData = await getAllResources();
-      console.log("copydata", swapiData);
-      // el método '.keys' de los objetos, nos permiten extraer solo
-      // la key del par key:value de un objeto
-      setResources(Object.keys(swapiData.data));
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	// 2. Definimos una funcion de flecha asincrona para
+	// traer los datos de la API y asignarlos a nuestra variable
+	const getSwapi = async () => {
+		try {
+			const swapiData = await getAllResources();
+			console.log("copydata", swapiData);
+			// el método '.keys' de los objetos, nos permiten extraer solo
+			// la key del par key:value de un objeto
+			setResources(Object.keys(swapiData.data));
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  // 3. Hacemos uso del gancho useEffect para llamar a nuestra funcion async
-  // solamente cuando se carga la pagina por primera vez.
-  // (por eso los [] a final)
-  useEffect(() => {
-    getSwapi();
-  });
+	// 3. Hacemos uso del gancho useEffect para llamar a nuestra funcion async
+	// solamente cuando se carga la pagina por primera vez.
+	// (por eso los [] a final)
+	useEffect(() => {
+		getSwapi();
+	});
 
-  return <div>Home</div>;
+	return (
+		<div>
+			<form action="">
+				<select name="" id="">
+					{resources.length > 0 && resources.map((value, index) => (
+						<option key={index} value={value}>{value}</option>
+					))}
+				</select>
+			</form>
+		</div>
+	)
 };

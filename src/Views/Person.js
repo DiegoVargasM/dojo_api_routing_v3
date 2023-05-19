e7f6b9926250d65a2d7fc59b5b0e1849c27c3603
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-18 20:33:24 -0500

    Add id routing functionality

--- src/Views/Person.js
+++ src/Views/Person.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export const Person = () => {
-	return (
-		<div>Person</div>
-	)
+    // 12. obtenemos el parametro "id" de la URL usando useParams
+    const { id } = useParams();
+
+    // 13. variable de estado para almacenar datos de persona
+    const [person, setPerson] = useState(null);
+
+    // 14. realizamos la solicitud cada que cambie el id con useEffect
+    useEffect(() => {
+        const fetchPerson = async () => {
+            try {
+                const response = await axios.get(`https://swapi.dev/api/people/${id}`)
+                setPerson(response.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetchPerson()
+    }, [id])
+
+    return (
+        <div>
+            <h2>{person.name}</h2>
+            {Object.entries(person).map(([key, value]) => (
+                <p key={key}>
+                    {key}: {value}
+                </p>
+            ))}
+        </div>
+    );
 }

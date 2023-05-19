b6c12b249a983fe322bc266ecbe70c5d3a5c3ec8
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-18 20:21:24 -0500

    Create Person component and route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom"
 import { Home } from "./Views/Home";
+import { Person } from "./Views/Person";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Person />} />
       </Routes>
     </div>
   )
